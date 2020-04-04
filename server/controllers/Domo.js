4f099b9787b8684fcfbbbf965c814ccf3378b776
jrc3123 <jrc3123@rit.edu>
2020-04-03 20:06:04 -0400

    Updating to fix ESLint errors

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -1,17 +1,16 @@
+const models = require('../models');
+
+const { Domo } = models;
+
 const makerPage = (req, res) => {
   Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
     if (err) {
       console.log(err);
-      return res.status(400).json({ error: 'An error occurred'});
+      return res.status(400).json({ error: 'An error occurred' });
     }
-    res.render('app', { domos: docs});
-  })
- 
+    return res.render('app', { domos: docs });
+  });
 };
-
-const models = require('../models');
-
-const { Domo } = models;
 
 const makeDomo = (req, res) => {
   if (!req.body.name || !req.body.age) {
