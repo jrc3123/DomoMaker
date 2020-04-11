1ba2edd6a62361d08c5413f5e38cc9bf6f0f3d67
jrc3123 <jrc3123@rit.edu>
2020-04-10 22:02:08 -0400

    Part D Done
    implemented React

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -6,9 +6,10 @@ const loginPage = (req, res) => {
   res.render('login', { csrfToken: req.csrfToken() });
 };
 
+/* No longer need this because we are using React
 const signupPage = (req, res) => {
   res.render('signup', { csrfToken: req.csrfToken() });
-};
+}; */
 
 const logout = (req, res) => {
   req.session.destroy();
@@ -37,6 +38,7 @@ const login = (request, response) => {
     return res.json({ redirect: '/maker' });
   });
 };
+
 
 const signup = (request, response) => {
   const req = request;
@@ -83,8 +85,19 @@ const signup = (request, response) => {
   });
 };
 
+const getToken = (request, response) => {
+  const req = request;
+  const res = response;
+
+  const csrfJSON = {
+    csrfToken: req.csrfToken(),
+  };
+
+  res.json(csrfJSON);
+};
+
 module.exports.loginPage = loginPage;
 module.exports.login = login;
 module.exports.logout = logout;
-module.exports.signupPage = signupPage;
 module.exports.signup = signup;
+module.exports.getToken = getToken;
